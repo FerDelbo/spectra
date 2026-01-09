0cb2209f517e7af957032b15e6002535f697f4ab
JoseVitorSAmorim <jose.santana.amorim@escola.pr.gov.br>
2026-01-09 20:28:53 -0300

    Melhorando Sistema de F.O. parte 3

--- spectra_1.1/prof/models.py
+++ spectra_1.1/prof/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-# Create your models here.
-class Professor(models.Model):
-    id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100)
-    usuario = models.CharField(max_length=50, unique=True)
-    senha = models.CharField(max_length=128)
-
-    REQURED_FIELDS = ['nome', 'usuario', 'senha']
 
 class UserType(models.Model):
     TIPO_CHOICES = [
@@ -25,17 +14,8 @@ class UserType(models.Model):
         return self.nome
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
     user_type = models.ForeignKey(UserType, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Tipo de Usuário")
 
     def __str__(self):
-        return f"{self.user.username} - {self.user_type}"
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+        return f"{self.user.username} - {self.user_type if self.user_type else 'Sem Tipo'}"
