2f8074f1b96dd94360ecc3919df07c8a7f321a5e
JoseVitorSAmorim <jose.santana.amorim@escola.pr.gov.br>
2026-01-23 09:43:17 -0300

    Melhorando Sistema de F.O. parte 9

--- spectra_1.1/home/views.py
+++ spectra_1.1/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Aluno, Turma
+
 from .models import FO
 # Create your views here.
 @login_required
 def home_view(request):
     fos_recentes = FO.objects.filter(usuario=request.user).order_by('-data_registro')[:5]
     return render(request, template_name='dashboard.html', context={'fos_recentes': fos_recentes})
+
+@login_required
+def lista_alunos(request, turma_id):
+    turma_especifica = Turma.objects.get(id=turma_id)
+    alunos = Aluno.objects.filter(turma=turma_especifica)
