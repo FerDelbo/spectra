507979646a7e21ea84d106ae8f4eb2d1293ab303
JoseVitorSAmorim <jose.santana.amorim@escola.pr.gov.br>
2026-03-16 18:32:00 -0300

    Update settings.py

--- spectra_1.1/spectra/settings.py
+++ spectra_1.1/spectra/settings.py
@@ -11,13 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, Csv
+from decouple import config, Csv, RepositoryEnv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ENV_PATH = BASE_DIR.parent.parent / '.env'
 
+config = Config(RepositoryEnv(ENV_PATH))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
