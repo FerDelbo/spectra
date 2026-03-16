d4734c7e362871224678adb579a6b9dfff280cb7
JoseVitorSAmorim <jose.santana.amorim@escola.pr.gov.br>
2026-03-16 17:47:00 -0300

    Ajustando

--- spectra_1.1/spectra/settings.py
+++ spectra_1.1/spectra/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
