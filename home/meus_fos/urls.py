ed360e0a29af7ae2728152e3ba8ce2725755e345
JoseVitorSAmorim <amorim.josevitor@gmail.com>
2026-06-05 17:06:54 -0300

    Correção da mecânica de anexo

--- home/meus_fos/urls.py
+++ home/meus_fos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from processo import views as processo_views
 from . import views
 
 urlpatterns = [
     path('', views.meus_fos, name='meus_fos'),
     path('historico/<int:aluno_id>/', views.historico_aluno, name='historico_aluno'),
     path('observacao/<int:fo_id>/', views.observacao_detalhes, name='observacao_detalhes'),
-    path('media/anexo/<path:path>', views.baixar_anexo_seguro, name='baixar_anexo_seguro'),
+    path('media/anexo/<path:path>', processo_views.baixar_anexo_seguro, name='baixar_anexo_seguro'),
 ]
